9035de4c6d7eadcb743b4482ee1518bec43d63cc
Alex Miller <apmiller@covermymeds.com>
2020-09-04 15:59:04 -0400

    Adds location, date and status to detail

--- src/views/spacex/LaunchesItemDetail.vue
+++ src/views/spacex/LaunchesItemDetail.vue
@@ -4,6 +4,34 @@
       <div class="uk-article-title">
         {{ missionName }}
       </div>
+      <div class="meta">
+        <p class="uk-article-meta launch-time-and-place">
+          <span>
+            <a
+              class="uk-link-muted icon-link"
+              :href="googleMapsUrl"
+              target="_blank"
+            >
+              <span>{{ launchSiteName }}</span>
+              <span class="small-icon">
+                <ExternalLinkIcon />
+              </span>
+            </a>
+          </span>
+          <span>{{ launchDateUtc | date({ utc: true }) }}</span>
+        </p>
+        <div>
+          <span
+            class="uk-label"
+            :class="{
+              'uk-label-success': missionSuccess,
+              'uk-label-danger': !missionSuccess
+            }"
+          >
+            {{ missionStatus }}
+          </span>
+        </div>
+      </div>
     </article>
     <alert
       v-else
@@ -19,10 +47,11 @@
 <script>
 import SpacexLaunchResolvable from "@/mixins/SpacexLaunchResolvable";
 import Alert from "@/components/Alert";
+import { ExternalLinkIcon } from "@/assets/icons/icons";
 
 export default {
   name: "LaunchesItemDetail",
-  components: { Alert },
+  components: { Alert, ExternalLinkIcon },
   mixins: [SpacexLaunchResolvable],
   props: {
     flightNumber: {
@@ -31,8 +60,29 @@ export default {
     }
   },
   computed: {
+    googleMapsUrl() {
+      return `https://maps.google.com/?q=${this.launchSiteName}`;
+    },
+    launchDateUtc() {
+      return this.launch.launch_date_utc;
+    },
+    launchDetails() {
+      return this.launch.details;
+    },
     missionName() {
       return this.launch.mission_name;
+    },
+    launchSiteName() {
+      return this.launch.launch_site?.site_name_long;
+    },
+    missionSuccess() {
+      return this.launch.launch_success;
+    },
+    missionStatus() {
+      return this.missionSuccess ? "Success" : "Failure";
+    },
+    youtubeId() {
+      return this.launch.links?.youtube_id;
     }
   },
   methods: {
