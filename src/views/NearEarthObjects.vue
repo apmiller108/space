eac2d3180a1563a1695ca2095c920fff120d3bcb
Alex Miller <apmiller@covermymeds.com>
2020-10-16 15:20:17 -0400

    Adds sun and mercury

--- src/views/NearEarthObjects.vue
+++ src/views/NearEarthObjects.vue
@@ -8,29 +8,69 @@ import * as THREE from "three";
 export default {
   name: "NearEarthObjects",
   mounted() {
+    // setup the scene, camera and render
     const scene = new THREE.Scene();
+
+    const fov = 40;
+    const near = 0.1;
+    const far = 1000;
     const camera = new THREE.PerspectiveCamera(
-      75,
+      fov,
       window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+      near,
+      far
     );
+    camera.position.z = 120;
+    camera.position.y = 0;
+
     const renderer = new THREE.WebGLRenderer();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     this.$refs.neo.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
+    // Create geometry, add to scene and move camera
+    const radius = 5;
+    const widthSegments = 32;
+    const heightSegments = 32;
+    const geometry = new THREE.SphereBufferGeometry(
+      radius,
+      widthSegments,
+      heightSegments
+    );
+    const material = new THREE.MeshBasicMaterial({
+      color: "#FAF194",
+      flatShading: false
+    });
+    const sun = new THREE.Mesh(geometry, material);
+    scene.add(sun);
 
+    const color = "#FFFFFF";
+    const intensity = 2;
+    const light = new THREE.PointLight(color, intensity);
+    light.position.set(0, 0, 0);
+    scene.add(light);
+
+    {
+      const radius = 1;
+      const widthSegments = 12;
+      const heightSegments = 12;
+      const geometry = new THREE.SphereBufferGeometry(
+        radius,
+        widthSegments,
+        heightSegments
+      );
+      const material = new THREE.MeshPhongMaterial({
+        color: "#B1B3B8",
+        flatShading: false
+      });
+      const mercury = new THREE.Mesh(geometry, material);
+      mercury.position.set(15, 0, 0);
+      scene.add(mercury);
+    }
+
+    // animate the scene
     function animate() {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      sun.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
     animate();
